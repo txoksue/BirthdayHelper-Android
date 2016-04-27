fcb2ae11c16ef6ce953662f809761129b920dcd8
txoksue <txoksue80@gmail.com>
2016-04-27 14:04:41 +0200

    Update Actions.java
    Añadidos comentarios sobre la función de los métodos.

--- src/main/java/com/example/joseramonparreno/birthdayhelper/Actions.java
+++ src/main/java/com/example/joseramonparreno/birthdayhelper/Actions.java
@@ -15,6 +15,12 @@ import java.util.ArrayList;
 /**
  * Created by txoksue on 4/4/16.
  */
+ 
+ /*********************************************
+ * Clase que implementa todos los métodos que
+ * ejecutan alguna acción contra la agenda.
+ *********************************************/
+ 
 public class Actions{
 
 
